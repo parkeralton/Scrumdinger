a3fe22ab141f4d3f6657dcbbfaf1e07a26fb08bc
Parker Alton <parkeralton@gmail.com>
2023-08-02 10:32:03 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by York MacBook 043 on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
